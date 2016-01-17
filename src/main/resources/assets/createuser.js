1be6fbe03534cd40f64e5b48179bb9504e93f70a
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2016-01-16 23:53:53 -0800

    require confirmation click when setting up user

--- src/main/resources/assets/createuser.js
+++ src/main/resources/assets/createuser.js
@@ -7,9 +7,7 @@ function createUser() {
     xmlHttp.open( "PUT", "api/createuser/" + twitchToken, false ); // false for synchronous request
     xmlHttp.send( null );
     var response = JSON.parse(xmlHttp.responseText);
-    document.getElementById('message').innerHTML = "Successfully set up user \"" + response.name + "\". Now paste this token into the tracker options: ";
+    document.getElementById('message').innerHTML = "Successfully set up user \"" + response.name + "\". Now paste this authkey into the tracker options: ";
     document.getElementById('token').value = response.token;
     console.log();
 }
-
-window.onload = createUser;
