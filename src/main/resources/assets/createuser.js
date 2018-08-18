118b2a89d46692449f12d1e00221290c347fb164
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2018-08-18 02:28:11 -0700

    write a legible error message in the browser when createuser fails

--- src/main/resources/assets/createuser.js
+++ src/main/resources/assets/createuser.js
@@ -7,7 +7,16 @@ function createUser() {
     xmlHttp.open( "PUT", "api/createuser/" + twitchToken, false ); // false for synchronous request
     xmlHttp.send( null );
     var response = JSON.parse(xmlHttp.responseText);
-    document.getElementById('message').innerHTML = "Successfully set up user \"" + response.name + "\". Now paste this authkey into the tracker options: ";
-    document.getElementById('token').value = response.token;
+    var messageNode = document.getElementById('message');
+    messageNode.innerHTML = "";
+    var text;
+    if ("name" in response) {        
+        text = document.createTextNode("Successfully set up user \"" + response.name + "\". Now paste this authkey into the tracker options: ");
+        document.getElementById('token').value = response.token;
+    } else {
+        text = document.createTextNode("The isaac tracker server gave an error; please report this bug. Error code: " + response.code + ". Message: \"" + response.message + "\"");        
+    }
+    messageNode.appendChild(text);
+    
     console.log();
 }
